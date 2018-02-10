42b860a942bd4cc837bcd2194f68cccb233051f0
BlackFox <BlackFox>
2018-02-10 16:48:45 +0800

    创建和短信验证相关的实体类

---
+++ src/org/evilcode/model/pojo/MsgReg.java
@@ -0,0 +1,12 @@
+package org.evilcode.model.pojo;
+
+public class MsgReg {
+	
+	private String sid; 
+	private String token;
+	private String appid;
+	private String templateid;
+	private String param;
+	private String mobile;
+	private String uid;
+}
