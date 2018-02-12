ba216da15d09eb419b4434f760fd067ccbf18c10
blackfox <huangy_tao@126.com>
2018-02-12 21:49:05 +0800

    在接口中定义和登录相关的抽象方法

--- src/org/evilcode/model/service/IUserInfo.java
+++ src/org/evilcode/model/service/IUserInfo.java
@@ -1,12 +1,14 @@
 package org.evilcode.model.service;
 
+import org.evilcode.model.pojo.Result;
 import org.evilcode.model.pojo.User;
 
 public interface IUserInfo {
 	
-	User getUser(int id);
 	
 	boolean isPhoneExist(String phoneNum);
+	
+	Result isLoginSuccess(User user);
 	
 	void addUser(User user);
 
