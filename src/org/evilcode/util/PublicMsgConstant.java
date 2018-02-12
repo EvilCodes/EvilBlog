1014b285bbb8ce1479380fdd7bd8d9badec93b0e
blackfox <huangy_tao@126.com>
2018-02-12 16:46:19 +0800

    创建用于存储业务状态信息的全局接口

---
+++ src/org/evilcode/util/PublicMsgConstant.java
@@ -0,0 +1,26 @@
+package org.evilcode.util;
+/**
+ * 
+ * @author blackfox
+ * 类描述：全局静态常量的引用接口
+ *
+ */
+public interface PublicMsgConstant {
+	
+	
+//	  注册成功的状态码
+	public static final int REGISTER_STATUS_SUCCESS=800; 
+	
+	
+//	登录成功的状态码
+	public static final int LOGIN_STATUS_SUCCESS=700;
+	
+	
+	//服务器返回信息的状态码
+	public static final int RESPONSE_OK=200;
+	public static final int PAGE_NOT_FOUND=404;
+	public static final int SERVICE_ERROR=500;
+	public static final int DATA_NOT_FOUND=401;
+	
+
+}
